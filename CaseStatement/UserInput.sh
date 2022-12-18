05bde1b358b1c1dbb3cc2c267f4d6cdbbb99a1fa
Ramkrishna <ramkrishna2k3@gmail.com>
2022-12-18 22:34:40 +0530

    added CaseStatement

--- CaseStatement/UserInput.sh
+++ CaseStatement/UserInput.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-echo -n "Enter country's name: "
-read COUNTRYNAME
+# echo -n "Enter country's name: "
+# read COUNTRYNAME
 
-echo -n "The Official Language of $COUNTRYNAME" is
+# echo -n "The Official Language of $COUNTRYNAME" is : ""
 
-case $COUNTRYNAME in
+# case $COUNTRYNAME in
 
-  India)
-    echo -e " Hindi\n"
-    ;;
-  Russia)
-    echo -e " Russian\n"
-    ;;
-  Bangaladesh)
-    echo -e " Urdu\n"
-    ;;
-  USA | "United Kingdom" | Australia | "SouthAfrica")
-    echo -e " English\n"
-    ;;
+#   India)
+#     echo -e "Hindi\n"
+#     ;;
+#   Russia)
+#     echo -e "Russian\n"
+#     ;;
+#   Bangaladesh)
+#     echo -e "Urdu\n"
+#     ;;
+#   USA | "United Kingdom" | Australia | "SouthAfrica")
+#     echo -e "English\n"
+#     ;;
+#    *)
+#     echo " unknown"
+#    ;;
 
-esac
+#esac
+
+#"or"
+
+echo "Do you know Shell Scripting?"
+read -p "Yes/No/Y/y/N/n? : " Input
+case $Input in
+    y|Y|Yes|yes)
+       echo "Ram learnt Shell Scripting"
+       echo 
+       ;;
+    No|N|no|n)
+      echo -e "Not a Big Deal"
+      ;;
+    esac
